2cbd8e760b7a707735baa89b17a1cda146039621
Hector Wiklund <Hector.Wiklund@sek.se>
2022-11-07 13:39:02 +0100

    Add project files.

---
+++ Selenium_Automation/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+IWebDriver driver = new ChromeDriver(@"C:\Users\hewd\Downloads\chromedriver.exe");
+
+driver.Url = @"https://cpm-utv01.sek.se/";
